9a0137b6a0e44420489d1a76af83409580463ddb
Nataliia <nvasinaa@gmail.com>
2022-07-15 13:29:52 +0300

    Задача 43

---
+++ 006/Program.cs
@@ -0,0 +1,30 @@
+﻿// написать программу которая найдет точку пересечения двух прямых
+
+        void Zadacha43()
+        {
+            Console.WriteLine("введите первое значение b1: ");
+            double b1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите второе значение k1: ");
+            double k1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите второе значение b2: ");
+            double b2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите второе значение k2: ");
+            double k2 = Convert.ToInt32(Console.ReadLine());
+            double x;
+            double y;
+
+            if (k1 != k2)
+            {
+                x = ((b2 - b1) / (k1 - k2));
+            Console.WriteLine("точка пересечения x равна: " + x);
+            y = (k2 * x + b2);
+             Console.WriteLine("точка пересечения y равна: " + y);
+             }
+             else Console.WriteLine("прямые параллельны");
+            
+
+            
+
+        }
+
+        Zadacha43();
